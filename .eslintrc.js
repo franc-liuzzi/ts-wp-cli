d14162242536fa3795b03b2769851d4b2091ddac
Francesco Liuzzi <francesco.liuzzi@sideagroup.com>
2020-11-04 17:57:36 +0100

    init

---
+++ .eslintrc.js
@@ -0,0 +1,51 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+    'import'
+  ],
+  rules: {
+    "arrow-body-style": 0,
+    "max-len": ["error", {
+      code: 120
+    }],
+    "import/no-unresolved": "error",
+    "comma-dangle": ["error", "always-multiline"],
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        "js": "never",
+        "jsx": "never",
+        "ts": "never",
+        "tsx": "never"
+      }
+    ]
+  },
+  "settings": {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"]
+    },
+    "import/resolver": {
+      // use <root>/tsconfig.json
+      "typescript": {
+        "alwaysTryTypes": true // always try to resolve types under `<roo/>@types` directory even it doesn't contain any source code, like `@types/unist`
+      }
+    }
+  }
+};
---
+++ test/index.ts
@@ -0,0 +1,14 @@
+/* eslint-disable */
+import WpCli from "../src";
+
+async function main() {
+
+
+  try {
+
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+main();
