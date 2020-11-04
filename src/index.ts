b58ca61015a7d40b6dbe805efd3ce3a3f01f8d74
Francesco Liuzzi <francesco.liuzzi@sideagroup.com>
2020-11-04 20:37:05 +0100

    fix package.json and npm-shrinkwrap.json

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
+import { defaults } from 'lodash';
+
+interface wpCliOptions {
+  wpCommand?: String,
+}
 
 export default class WpCli {
 
+  options: wpCliOptions = {
+    wpCommand: 'wp',
+  };
+
+  construct(options: wpCliOptions = {}) {
+    this.options = defaults(options, this.options);
+  }
 }
